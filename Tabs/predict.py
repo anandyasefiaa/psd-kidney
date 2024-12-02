72440f5377b3421ca45c44914bbe0fed55897f36
anandyasefiaa <sefiaanandya@gmail.com>
2024-12-02 07:53:39 +0700

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -17,36 +17,20 @@ def app(df, x, y):
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        bp = st.text_input('Input Nilai bp')
-        sg = st.text_input('Input Nilai sg')
-        al = st.text_input('Input Nilai al')
-        su = st.text_input('Input Nilai su')
-        rbc = st.text_input('Input Nilai rbc')
-        pc = st.text_input('Input Nilai pc')
-        pcc = st.text_input('Input Nilai pcc')
-        ba = st.text_input('Input Nilai ba')
+        sg = st.text_input('Input Nilai Specific Gravity ')
+        al = st.text_input('Input Nilai Albumin')
+        pc = st.text_input('Input Nilai Pus Cell(normal)')
 
     with col2:
-        bgr = st.text_input('Input Nilai bgr')
-        bu = st.text_input('Input Nilai bu')
-        sc = st.text_input('Input Nilai sc')
-        sod = st.text_input('Input Nilai sod')
-        pot = st.text_input('Input Nilai pot')
-        hemo = st.text_input('Input Nilai hemo')
-        pcv = st.text_input('Input Nilai pcv')
-        wc = st.text_input('Input Nilai wc')
+        bgr = st.text_input('Input Nilai Blood Glucose Random')
+        hemo = st.text_input('Input Nilai Hemoglobin')
 
     with col3:
-        rc = st.text_input('Input Nilai rc')
-        htn = st.text_input('Input Nilai htn')
-        dm = st.text_input('Input Nilai dm')
-        cad = st.text_input('Input Nilai cad')
-        appet = st.text_input('Input Nilai appet')
-        pe = st.text_input('Input Nilai pe')
-        ane = st.text_input('Input Nilai ane')
+        htn = st.text_input('Input Nilai Hypertension')
+        dm = st.text_input('Input Nilai DIabetes Mellitus')
 
     # Masukkan fitur ke dalam list dan ubah ke tipe numerik
-    features = [bp, sg, al, su, rbc, pc, pcc, ba, bgr, bu, sc, sod, pot, hemo, pcv, wc, rc, htn, dm, cad, appet, pe, ane]
+    features = [sg, al, pc, bgr, hemo, htn, dm]
     numeric_features = []
 
     for feature in features:
@@ -57,14 +41,14 @@ def app(df, x, y):
             numeric_features.append(0.0)  # Jika tidak bisa dikonversi, ganti dengan 0
 
     # Prediksi ketika tombol diklik
-    if st.button("Prediksi"):
+    if st.button("Klasifikasi"):
         if model_choice == "Decision Tree":
             prediction, score = predict(x, y, numeric_features)
         elif model_choice == "Naive Bayes":
             model, score = train_naive_bayes(x, y)
             prediction = model.predict([numeric_features])  # Pastikan numeric_features adalah 2D array
 
-        st.info("Prediksi Sukses...")
+        st.info("Klasifikasi Sukses...")
 
         if prediction == 1:
             st.warning("Orang tersebut rentan terkena penyakit ginjal.")
