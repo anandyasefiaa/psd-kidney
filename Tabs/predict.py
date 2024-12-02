c87d1ed1345ec351b6875967b82fb0aa936bcdbd
anandyasefiaa <sefiaanandya@gmail.com>
2024-12-02 08:19:52 +0700

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -17,20 +17,36 @@ def app(df, x, y):
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        sg = st.text_input('Input Nilai Specific Gravity ')
-        al = st.text_input('Input Nilai Albumin')
-        pc = st.text_input('Input Nilai Pus Cell(normal)')
+        bp = st.text_input ('Input Nilai bp')
+        sg = st.text_input ('Input Nilai sg')
+        al = st.text_input ('Input Nilai al')
+        su = st.text_input ('Input Nilai su')
+        rbc = st.text_input ('Input Nilai rbc')
+        pc = st.text_input ('Input Nilai pc')
+        pcc = st.text_input ('Input Nilai pcc')
+        ba = st.text_input ('Input Nilai ba')
 
     with col2:
-        bgr = st.text_input('Input Nilai Blood Glucose Random')
-        hemo = st.text_input('Input Nilai Hemoglobin')
+        bgr = st.text_input ('Input Nilai bgr')
+        bu = st.text_input ('Input Nilai bu')
+        sc = st.text_input ('Input Nilai sc')
+        sod = st.text_input ('Input Nilai sod')
+        pot = st.text_input ('Input Nilai pot')
+        hemo = st.text_input ('Input Nilai hemo')
+        pcv = st.text_input ('Input Nilai pcv') 
+        wc = st.text_input ('Input Nilai wc')
 
     with col3:
-        htn = st.text_input('Input Nilai Hypertension')
-        dm = st.text_input('Input Nilai DIabetes Mellitus')
+        rc = st.text_input ('Input Nilai rc')
+        htn = st.text_input ('Input Nilai htn')
+        dm = st.text_input ('Input Nilai dm')
+        cad = st.text_input ('Input Nilai cad')
+        appet = st.text_input ('Input Nilai appet')
+        pe = st.text_input ('Input Nilai pe')
+        ane = st.text_input ('Input Nilai ane')
 
     # Masukkan fitur ke dalam list dan ubah ke tipe numerik
-    features = [sg, al, pc, bgr, hemo, htn, dm]
+    features = [bp,sg,al,su,rbc,pc,pcc,ba,bgr,bu,sc,sod,pot,hemo,pcv,wc,rc,htn,dm,cad,appet,pe,ane]
     numeric_features = []
 
     for feature in features:
